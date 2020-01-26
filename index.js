8d01553db50a45ca81aeb872ba500f8d73fea7f9
Devin Benson <devinbenson@gmail.com>
2020-01-26 16:50:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,14 @@ function dec(a){
 function makeInt('string'){
 return parseInt('string', 10)
 }
+
+
+
+unction sayHiToGrandma(string) {
+  if (string.toLowerCase()==string){
+  return 'I can\'t hear you!'}
+  if (string.toUpperCase()==string){
+  return 'YES INDEED!'}
+  if (string=="I love you, Grandma."){
+  return 'I love you, too.'}
+}
